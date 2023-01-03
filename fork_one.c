443f06d31e3095eca0d8fcb49263d47be2b40ada
Charan-singamsetty1504 <singamsettycharan046@gmail.com>
2023-01-03 11:28:38 +0530

    Basic Fork() and definition

--- fork_one.c
+++ fork_one.c
@@ -2,6 +2,12 @@
 #include<sys/types.h>
 #include<unistd.h>
 
+//fork is a system call which is used to create new process, and 
+//runs concurrently along with the parent process as a child process
+//it takes no parameters and returns an integer value
+//Negative Value - creation of child process was unsuccessful
+//Zero - returns to newly created child process
+//positive value - return to the parent or caller, The value contains the process ID
 
 int main()
 {
