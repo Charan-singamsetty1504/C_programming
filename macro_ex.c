875e7fb54af0a91af35af958eacaf1ea87e17f11
Charan-singamsetty1504 <118269672+Charan-singamsetty1504@users.noreply.github.com>
2023-03-09 23:05:57 +0530

    Create macro_ex.c

---
+++ macro_ex.c
@@ -0,0 +1,10 @@
+#include<stdio.h> 
+
+#define prod(a,b) a*b //added at the preprocessor stage 
+
+int main()
+{
+	int x=3,y=4;
+	printf("%d",prod(x+2,y-1)); //first the values will be assigned because the macros only expect integers and after it adds the some operations in code
+	return 0;
+}
